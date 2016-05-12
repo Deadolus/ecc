acb74c2bc1ff9b03adddb626a369dd6f1e5340d5
Simon Egli <simon.egli@bbv.ch>
2016-05-12 09:55:27 +0200

    Getting a grip on what is required
    Signed-off-by: Simon Egli <simon.egli@bbv.ch>

--- ecc/ecc.cc
+++ ecc/ecc.cc
@@ -17,6 +17,10 @@ inline void initialize() {
 
 namespace Ecc {
 
+  /// Sign some data with a private key
+  /// @param key Key with which to sign the hash
+  /// @param hash Hash (data) to be signed
+  /// @return Signature of the signed hash
 std::unique_ptr<ISignature> sign(std::unique_ptr<IKey> const & key, std::vector<uint8_t> const & hash) {
   initialize();
 
@@ -25,6 +29,10 @@ std::unique_ptr<ISignature> sign(std::unique_ptr<IKey> const & key, std::vector<
   return nullptr;
 }
 
+/// verify that the signature of a hash has been signed (with the correct private key) by the public key
+/// @param key public key to use for verifying
+/// @param hash Hash which was signed
+/// @param signature Received signature of signed hash
 bool verify(std::unique_ptr<IKey> const & key, std::vector<uint8_t> const & hash, std::unique_ptr<ISignature> const & signature) {
   initialize();
 
